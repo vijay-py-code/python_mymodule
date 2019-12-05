472fe8510b8401d4803c3344b098a607cc896140
vijay-py-code <58543474+vijay-py-code@users.noreply.github.com>
2019-12-05 13:45:47 +0800

    Create mymodule.py
    first python module code with a function

---
+++ mymodule.py
@@ -0,0 +1,29 @@
+# ===========
+# mymodule.py
+# ===========
+
+from urllib.request import urlopen
+import json
+
+api_key = "xxxxxxxx"
+
+def get_weather(city):
+    sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=")
+    result = sock.read()                            
+    sock.close()                                        
+    weather = json.loads(result)
+    return weather["main"]["temp"] -273.15
+
+def postal_lookup(postal_code):
+    sock = urlopen("http://api.postcodes.io/postcodes/" + postal_code)
+    result = sock.read()                            
+    sock.close()                                        
+    details = json.loads(result)
+    return (details["result"]["latitude"], details["result"]["longitude"])
+
+if __name__ == "__main__":
+    degrees = get_weather("SINGAPORE")
+    print("Weather in SINGAPORE is %.2f degrees Celsius" % degrees)
+
+    location = postal_lookup("B323PP")
+    print(location)
